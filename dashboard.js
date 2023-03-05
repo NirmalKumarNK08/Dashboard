498adf4527c040f46c1089c7e956934389f33162
NirmalKumarNK08 <nirmalkumar.nk008@gmail.com>
2023-03-05 21:49:13 +0530

    Updated

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,22 @@
 const flight = document.getElementById('plane');
 const c1 = document.getElementById('c1');
+const airportSchedule = document.getElementById('toggle');
+const dashboard = document.getElementById('dashboard');
+const title = document.getElementById('title');
+const searchBar = document.getElementById('searchBar');
+const boards = document.getElementById("boards");
+
 
 
 window.addEventListener('scroll', () => {
    let value = window.scrollY;
    flight.style.left = value + 'px';
    flight.style.bottom = value + 'px';
+})
+
+airportSchedule.addEventListener('click', () => {
+   dashboard.classList.add('active');
+   title.classList.add("active");
+   searchBar.classList.add("active");
+   boards.classList.add("active");
 })
