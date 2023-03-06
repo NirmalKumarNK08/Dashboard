153865d79d7fea522138630da007b1e6f450d3d1
NirmalKumarNK08 <nirmalkumar.nk008@gmail.com>
2023-03-06 22:40:06 +0530

    Updated

--- dashboard.js
+++ dashboard.js
@@ -15,7 +15,13 @@ const searchBar1 = document.getElementById('searchBar1');
 const boards = document.getElementById("boards");
 const boards1 = document.getElementById("boards1");
 
+const charactersList = document.getElementById("charactersList");
+const charactersList1 = document.getElementById("charactersList1");
+const charactersList2 = document.getElementById("charactersList2");
 
+const org = document.getElementById("origin");
+const destination = document.getElementById("destination");
+const airport = document.getElementById("airport");
 
 window.addEventListener('scroll', () => {
    let value = window.scrollY;
@@ -54,3 +60,109 @@ flightSchedule.addEventListener('click', () => {
    airportSchedule.classList.remove("active");
    flightSchedule.classList.add("active");
 })
+
+org.addEventListener("keyup", (e) => {
+  const searchString = e.target.value;
+  if (searchString != "") {
+    charactersList.classList.remove("active");
+  } else {
+    charactersList.classList.add("active");
+  }
+});
+
+destination.addEventListener("keyup", (e) => {
+  const searchString = e.target.value;
+  if (searchString != "") {
+    charactersList1.classList.remove("active");
+  } else {
+    charactersList1.classList.add("active");
+  }
+});
+
+airport.addEventListener("keyup", (e) => {
+  const searchString = e.target.value;
+  if (searchString != "") {
+    charactersList2.classList.remove("active");
+  } else {
+    charactersList2.classList.add("active");
+  }
+});
+
+let hpCharacters = [];
+function suggession(e) {
+   const searchString = e.target.value.toLowerCase();
+   // console.log(searchString);
+   const filteredCharacters = hpCharacters.filter((character) => {
+     return (
+       character.name.toLowerCase().includes(searchString) ||
+       character.country.toLowerCase().includes(searchString) ||
+       character.city.toLowerCase().includes(searchString) ||
+       character.iata_code.toLowerCase().includes(searchString)
+     );
+   });
+   // console.log(filteredCharacters);
+   displayCharacters(filteredCharacters);
+}
+org.addEventListener("keyup", (e) => {
+  suggession(e);
+});
+destination.addEventListener("keyup", (e) => {
+  suggession(e);
+});
+airport.addEventListener("keyup", (e) => {
+  suggession(e);
+});
+
+const loadCharacters = async () => {
+  try {
+    const res = await fetch("./Airport Codes/airports-code.json");
+    hpCharacters = await res.json();
+    displayCharacters(hpCharacters);
+    displayCharacters1(hpCharacters);
+    displayCharacters2(hpCharacters);
+    // console.log(hpCharacters);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const displayCharacters = (characters) => {
+  const htmlString = characters
+    .map((character) => {
+      return `
+         <li class="character">
+            <h3><span>✈ </span>${character.name}</h3>
+            <p class="code">${character.iata_code}</p>
+         </li>`;
+    })
+    .join("");
+  charactersList.innerHTML = htmlString;
+};
+
+const displayCharacters1 = (characters) => {
+  const htmlString = characters
+    .map((character) => {
+      return `
+         <li class="character">
+            <h3><span>✈ </span>${character.name}</h3>
+            <p class="code">${character.iata_code}</p>
+         </li>`;
+    })
+    .join("");
+  charactersList1.innerHTML = htmlString;
+};
+const displayCharacters2 = (characters) => {
+  const htmlString = characters
+    .map((character) => {
+      return `
+         <li class="character">
+            <h3><span>✈ </span>${character.name}</h3>
+            <p class="code">${character.iata_code}</p>
+         </li>`;
+    })
+    .join("");
+  charactersList2.innerHTML = htmlString;
+};
+
+loadCharacters();
+
