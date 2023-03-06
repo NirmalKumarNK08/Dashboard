909c9ea8895e867e701cdf1c86867d93e3a96078
NirmalKumarNK08 <nirmalkumar.nk008@gmail.com>
2023-03-06 11:10:42 +0530

    Updated and Uploading files

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,19 @@
 const flight = document.getElementById('plane');
 const c1 = document.getElementById('c1');
+
 const airportSchedule = document.getElementById('toggle');
+const flightSchedule = document.getElementById('toggle1');
+
 const dashboard = document.getElementById('dashboard');
+
 const title = document.getElementById('title');
+const title1 = document.getElementById('title1');
+
 const searchBar = document.getElementById('searchBar');
+const searchBar1 = document.getElementById('searchBar1');
+
 const boards = document.getElementById("boards");
+const boards1 = document.getElementById("boards1");
 
 
 
@@ -16,7 +25,32 @@ window.addEventListener('scroll', () => {
 
 airportSchedule.addEventListener('click', () => {
    dashboard.classList.add('active');
+
    title.classList.add("active");
+   title1.classList.remove("active");
+
    searchBar.classList.add("active");
+   searchBar1.classList.remove("active");
+
    boards.classList.add("active");
+   boards1.classList.remove("active");
+
+   airportSchedule.classList.add("active")
+   flightSchedule.classList.remove("active");
+})
+
+flightSchedule.addEventListener('click', () => {
+   dashboard.classList.remove("active");
+
+   title.classList.remove("active");
+   title1.classList.add("active");
+
+   searchBar.classList.remove("active");
+   searchBar1.classList.add("active");
+
+   boards.classList.remove("active");
+   boards1.classList.add("active");
+
+   airportSchedule.classList.remove("active");
+   flightSchedule.classList.add("active");
 })
